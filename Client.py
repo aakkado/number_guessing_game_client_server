1711899e273080de641eef1c5c461c85fcd00f4b
Matheus Lopes De Almeida Tiago <ht3016463@hto.ifsp.edu.br>
2023-10-03 19:25:40 -0300

    game update

--- Client.py
+++ Client.py
@@ -2,8 +2,26 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-guess = input("Enter your guess: ")
+while True:
+    guess = input("Enter your guess (or 'q' to quit): ")
 
-response = requests.get(BASE + "game/" + guess)
+    if guess.lower() in ['q', 'quit']:
+        print("Exiting the game. Goodbye!")
+        break
 
-print(response.json())
+    try:
+        guess = int(guess)
+        response = requests.get(BASE + "game/" + str(guess))
+        print(response.json())
+
+        if response.json() == "Correct! Type /restart to start a new game or /stop to stop.":
+            restart = input("Do you want to restart the game? (y/n): ")
+            if restart.lower() == 'y':
+                response = requests.get(BASE + "restart")
+                print(response.json())
+            else:
+                print("Goodbye!")
+                break
+
+    except ValueError:
+        print("Invalid input. Please enter a valid number or 'q' to quit.")
--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 import random
-
-answer = random.randint(1, 100)
 
 app = Flask(__name__)
 api = Api(app)
 
 class Game(Resource):
-    
+    def __init__(self):
+        self.answer = random.randint(1, 1000)
+        self.game_over = False
+
     def get(self, num):
-        if num == answer:
-            return "Correct!"
-        elif num < answer:
+        if self.game_over:
+            return "Game over. Start a new game using /restart"
+        
+        if num == self.answer:
+            self.game_over = True
+            return "Correct! Type /restart to start a new game or /stop to stop."
+
+        elif num < self.answer:
             return "Too low!"
         else:
             return "Too high!"
 
+class Restart(Resource):
+    def get(self):
+        game.answer = random.randint(1, 1000)
+        game.game_over = False
+        return "New game started. Guess a number between 1 and 1000."
+
+class Stop(Resource):
+    def get(self):
+        return "Game stopped. Goodbye!"
+
 api.add_resource(Game, "/game/<int:num>")
+api.add_resource(Restart, "/restart")
+api.add_resource(Stop, "/stop")
 
 if __name__ == "__main__":
+    game = Game()
     app.run(debug=True)
